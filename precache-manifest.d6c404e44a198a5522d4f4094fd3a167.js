eb6d80a0dfc1ad8d1022da222ae4b43d768114f6
gabrielforms <gabrielformiga3@gmail.com>
2020-02-11 17:39:54 -0300

    feat(master): instalação do ambiente

---
+++ precache-manifest.d6c404e44a198a5522d4f4094fd3a167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf82535dc50d2a5b5e63318e808e3151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f772f4478b841b79d4e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a2e84dc7818596582df7",
+    "url": "/static/js/2.ab6df954.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ab6df954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f772f4478b841b79d4e",
+    "url": "/static/js/main.a235ed77.chunk.js"
+  },
+  {
+    "revision": "79c01e090cbc359cb83e",
+    "url": "/static/js/runtime-main.5b03360a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
